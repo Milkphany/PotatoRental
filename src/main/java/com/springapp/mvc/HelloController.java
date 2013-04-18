4b233e7141d935ddafc41d56269927571bb0fce4
milkphany <milkphany@gmail.com>
2013-04-17 22:18:00 -0400

    Should go to gibhub!

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
-		return "hello";
+		return "Why not why why why";
 	}
 }
