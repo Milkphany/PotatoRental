de06620fa1025336310d1bf9d26b245830a6dc9c
Jason Chen <milkphany@gmail.com>
2013-04-21 17:44:08 -0400

    Testing out with UserController.

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -5,12 +5,16 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/*
 @Controller
 @RequestMapping("/")
+*/
 public class HelloController {
+/*
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Testing them!");
-		return "Why not why why why";
+		model.addAttribute("message", "It happens easily...");
+		return "hello";
 	}
+*/
 }
---
+++ src/main/java/com/springapp/mvc/UserController.java
@@ -0,0 +1,9 @@
+package com.springapp.mvc;
+
+/**
+ * User: Milky
+ * Date: 4/21/13
+ * Time: 5:42 PM
+ */
+public class UserController {
+}
