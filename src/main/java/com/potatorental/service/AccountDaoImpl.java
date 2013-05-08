3b4acb2a141e17463a6f093592f57fd708f77b44
Jason Chen <milkphany@gmail.com>
2013-05-08 18:58:33 -0400

    Fixed movie rentals

--- src/main/java/com/potatorental/service/AccountDaoImpl.java
+++ src/main/java/com/potatorental/service/AccountDaoImpl.java
@@ -50,7 +50,7 @@ public class AccountDaoImpl implements AccountDao {
 
     @Override
     public List<Movie> getHistory(Account account) {
-        String sql = "select m.name, m.id, p.datetime, p.returndate, p.id " +
+        String sql = "select m.name, m.id " +
                 "from rental r, purchase p, movie m " +
                 "where m.id = r.movieid and p.id = r.purchid and r.accountid = ?";
         return PotatoService.addMoviesFromMap(jdbcTemplate.queryForList(sql, account.getId()));
